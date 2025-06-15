705a6c288b8a0464f7c8882ba211cff665584f6f
saidamst2003 <saida2003mst@gmail.com>
2025-06-15 23:41:28 +0100

    j'ai creer passwordexeptio et custemexection

---
+++ deliverymatch/backend/src/main/java/delivery/example/backend/exception/PasswordIncorrectException.java
@@ -0,0 +1,7 @@
+package delivery.example.backend.exception;
+
+public class PasswordIncorrectException extends RuntimeException {
+    public PasswordIncorrectException(String message ) {
+        super( message );
+    }
+}
