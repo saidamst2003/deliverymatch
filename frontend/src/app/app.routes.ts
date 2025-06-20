b5057371e4bfbe47ab42f622552389cfb1c3235e
saidamst2003 <saida2003mst@gmail.com>
2025-06-20 23:44:43 +0100

    j'ai creer reting mour mes trajet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,23 @@ import { Routes } from '@angular/router';
 import {MesTrajet} from './components/pages/mes-trajet/mes-trajet';
 import {LayoutComponent} from './components/layout/layout/layout';
 export const routes: Routes = [
-
   {
-    path: "mes-trajet",
-    component: MesTrajet,
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'mes-trajet',
+        component: MesTrajet
+      },
+      {
+        path: '',
+        redirectTo: '/mes-trajet',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/mes-trajet'
   }
-]
+];
