e425facfa6fe2e8633afe5fb50a41ae6b8350da5
saidamst2003 <saida2003mst@gmail.com>
2025-06-21 13:47:13 +0100

    j'ai creer logique de login et html

--- frontend/src/app/components/pages/login/login.ts
+++ frontend/src/app/components/pages/login/login.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  loginForm: FormGroup;
+  errorMessage = '';
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.token);
+        this.router.navigate(['/home']);
+      },
+      error: () => {
+        this.errorMessage = 'Email ou mot de passe incorrect';
+      },
+    });
+  }
 }
