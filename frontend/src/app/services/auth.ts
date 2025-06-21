f23b2bde72ed0fd2adfd840be30e135335d99533
saidamst2003 <saida2003mst@gmail.com>
2025-06-21 13:44:08 +0100

    j'ai creer service  auth

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Auth {
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/user/login';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  login(data: { email: string; password: string }) {
+    return this.http.post<any>(this.apiUrl, data);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('jwt_token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('jwt_token');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('jwt_token');
+  }
 }
