4b6a1ddcb012e9e8349d57b718655f20d0b8b36a
saidamst2003 <saida2003mst@gmail.com>
2025-06-21 13:53:50 +0100

    j'ai creer composant guard

---
+++ frontend/src/app/guards/auth-guard.ts
@@ -0,0 +1,16 @@
+// src/app/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import {AuthService} from '../services/auth';
+
+export const authGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if (auth.isAuthenticated()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false;
+  }
+};
