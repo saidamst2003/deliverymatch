3074d021b5a8693122e49808043faa0c01b68b55
saidamst2003 <saida2003mst@gmail.com>
2025-06-21 13:49:39 +0100

    j'ai creer ts pour AuthInterceptor

--- frontend/src/app/interceptors/auth-interceptor.ts
+++ frontend/src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,21 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {  HttpInterceptor,  HttpRequest,  HttpHandler, HttpEvent,} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import {AuthService} from '../services/auth';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  private auth = inject(AuthService);
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + token),
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
